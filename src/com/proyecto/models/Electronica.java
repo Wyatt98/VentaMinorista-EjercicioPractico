d1737a06b8109add2cc1d55ca8dd22be1b367218
Herrera <Omar.Herrera@autozone.com>
2024-05-28 23:16:52 -0600

    Se agregaron subclases

---
+++ src/com/proyecto/models/Electronica.java
@@ -0,0 +1,18 @@
+package com.proyecto.models;
+
+import com.proyecto.enums.CategoriaProducto;
+
+public class Electronica extends Producto {
+
+	public Electronica(String nombre, float precio, CategoriaProducto categoria, String marca) {
+		super(nombre, precio, categoria, marca);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void mostrarDetalleProducto() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/com/proyecto/models/Ropa.java
@@ -0,0 +1,18 @@
+package com.proyecto.models;
+
+import com.proyecto.enums.CategoriaProducto;
+
+public class Ropa extends Producto{
+
+	public Ropa(String nombre, float precio, CategoriaProducto categoria, String marca) {
+		super(nombre, precio, categoria, marca);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void mostrarDetalleProducto() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
