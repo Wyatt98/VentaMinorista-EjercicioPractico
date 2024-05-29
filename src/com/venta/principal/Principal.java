1960e2ae1014085c7ca23885959e5663b9043872
Silva <Jose.Silva@autozone.com>
2024-05-28 22:29:32 -0600

    Paquetes principales creados

---
+++ src/com/venta/principal/Principal.java
@@ -0,0 +1,10 @@
+package com.venta.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Estatus inicial del proyecto");
+	}
+
+}
