c3ac8a151984dad0cf8763d7b1a8ed97b2df9e9c
Silva <Jose.Silva@autozone.com>
2024-05-31 23:50:41 -0600

    Programa principal terminado

--- src/com/venta/principal/Principal.java
+++ src/com/venta/principal/Principal.java
@@ -29,28 +29,35 @@ public class Principal {
 
 		System.out.println("Total de productos: " + Inventario.getTotalProductos() + "\n");
 
-		while (true) {
+		Scanner input = new Scanner(System.in);
+		boolean loop = true;
+		while (loop) {
 			System.out.println("Seleccione el tipo de búsqueda:\n");
-			System.out.println("1. Categoría\n" + "2. Rango de precios\n" + "3. Total de productos\n" + "4. Finalizar");
-			System.out.println();
+			System.out.println("1. Categoría\n" + "2. Rango de precios\n" + "3. Todos los productos\n" + "4. Finalizar \n");
+			System.out.print(">>> ");
 
-			Scanner input = new Scanner(System.in);
 			String eleccion = input.next();
 
 			switch (eleccion) {
 			case "1":
+				
+				System.out.println("Categorías disponibles. -");
+				for (CategoriaProducto categoriaproducto : CategoriaProducto.values())
+					System.out.println(categoriaproducto);
+				
 				System.out.print("Categoria: ");
 				String categoria = input.next();
 				System.out.println();
+				
 				for (CategoriaProducto categoriaproducto : CategoriaProducto.values()) {
 					if (categoria.equalsIgnoreCase(categoriaproducto.name()))
 						inventario.productosPorCategoria(categoriaproducto);
 
 				}
+				break;
 				
 			case "2":
-				double inferior;
-				double superior;
+				double inferior, superior;
 				
 				System.out.print("Rango inferior: ");
 				inferior = Double.parseDouble(input.next());
@@ -58,13 +65,32 @@ public class Principal {
 				System.out.print("Rango superior: ");
 				superior = Double.parseDouble(input.next());
 				
+				inventario.productosPorRango(inferior, superior);
+				break;
 				
+			case "3":
+				inventario.mostrarTodosLosProductos();
+				break;
+				
+			case "4":
+				loop = false;
+				break;
+				
+			default:
+				System.out.println("Opción no válida.");
+				break;
 			}
 			
 			System.out.println("---------------------------------------\n");
 			
-			input.close();
+			System.out.print("Continuar? (y/n): ");
+			if (input.next().equalsIgnoreCase("n"))
+				loop = false;
+			
+			
 		}
+		input.close();
+		System.out.println("Programa terminado.");
 
 	}
 
